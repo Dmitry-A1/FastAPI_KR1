df39249eb4546fd848956523190897eca076a829
Dima Anurov <anurov.dm@gmail.com>
2026-02-18 15:32:12 +0300

    Task 2.2

--- Task2_2/main.py
+++ Task2_2/main.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+import re
 
-feedback_message = []
+
 class Feedback(BaseModel):
-    name: str
-    message: str
+    name: str = Field(min_length = 2, max_length = 50)
+    message: str = Field(min_length = 10, max_length = 500)
+
+    @field_validator("message")
+    def message_received(cls, value):
+        plohie_slova = ["кринж","рофл","вайб"]
+        patter = re.compile(rf"\b({'|'.join(plohie_slova)})\w*\b", re.IGNORECASE)
+
+        if patter.search(value):
+            raise ValueError("Использование недопустимых слов")
+        return value
 
 app = FastAPI()
+
+feedbacks = []
 @app.post("/feedback")
 async def feedback(feedback: Feedback):
-    feedback_message.append ({
+    feedbacks.append ({
         "name":feedback.name,
         "message":feedback.message
     })
     return {
-        "message": f"Feedback received. Thank you,{feedback.name}!"
+        "message": f"Спасибо, {feedback.name}! Ваш отзыв сохранен."
     }
